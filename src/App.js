c85bc4dee3d4d6c78d543fa1864d1331444b9cd6
박건규 <geongyu09@gmail.com>
2025-10-18 17:23:58 +0900

    feat(app): 사용자 입력 처리 로직 구현
    사용자의 입력 처리 로직을 app.js에 추가하였습니다.
    요구사항에 맞추어서 @woowacourse/mission-utils의 Console.readLineAsync 를 사용하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await Console.readLineAsync(
+        "덧셈할 문자열을 입력해 주세요.\n",
+      );
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
 }
 
 export default App;
