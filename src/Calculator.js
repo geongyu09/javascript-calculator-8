edba95356ae15dc387f9b00ca442c4015cadd74c
박건규 <geongyu09@gmail.com>
2025-10-18 20:19:17 +0900

    feat(Calculator): 커스텀 구분자를 사용한 경우 처리 로직 구현
    커스텀 구분자를 사용한 경우의 로직을 구현하였습니다.
    구분자의 사용 여부는 "//" 로 시작하는지에 대한 여부입니다.
    자세한 구현 사항은 아래와 같습니다.
    - "\\n"이 나오기 이전과 이후로 구분하여 ("//" 부터) 나오기 이전까지의 문자들을 구분자로 판단
    - 구분자를 기준으로 .split()을 통하여 분리
    - 각각을 더해서 반환
    
    여기서 더하는 연산 중 NaN이 아닌 숫자형으로 올바르게 변환되지 않은 경우 부적절한 글자가 포함된 것으로 판단하였습니다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,5 +4,24 @@ export class Calculator {
   calculate(inputString) {
     // 빈 문자열인 경우
     if (inputString === "") return 0;
+
+    // 커스텀 구분자를 사용하는 경우
+    if (inputString.startsWith("//")) {
+      const index = inputString.indexOf("\\n");
+
+      if (index === -1)
+        throw new Error(
+          "[ERROR] 올바르게 커스텀 구분자가 설정되지 않았습니다.\n\\n누락",
+        );
+
+      const separator = inputString.slice(2, index);
+      const input = inputString.slice(index + 2);
+
+      if (input?.length === 0) return 0;
+      const result = input.split(separator).reduce((sum, cur) => +sum + +cur);
+      if (Number.isNaN(result))
+        throw new Error("[ERROR] 부적절한 문자가 들어갔습니다.");
+      return result;
+    }
   }
 }
