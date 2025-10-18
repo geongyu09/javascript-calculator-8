224cc9aa8f43608002dc8a60b4bacc675ebd5033
박건규 <geongyu09@gmail.com>
2025-10-18 23:05:19 +0900

    feat(NumberBuffer): NumberBuffer 클래스 구현
    0-9의 정수를 넣고 이를 하나의 나열된 수를 얻을 수 있는 NumberBuffer 클래스를 구현하였습니다.

---
+++ src/NumberBuffer.js
@@ -0,0 +1,23 @@
+export class NumberBuffer {
+  #digits = [];
+  constructor() {}
+
+  isEmpty() {
+    return this.#digits.length === 0;
+  }
+
+  appendNumber(nbr) {
+    if (typeof nbr !== "number" || nbr < 0 || nbr > 9 || Number.isNaN(nbr))
+      throw new Error("올바른 인자가 아닙니다");
+    this.#digits.push(nbr);
+  }
+
+  toNumber() {
+    if (this.isEmpty()) return 0;
+    return parseInt(this.#digits.map(String).join(""));
+  }
+
+  clear() {
+    this.#digits = [];
+  }
+}
