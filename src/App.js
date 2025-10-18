958e8fb31349ecc3b945dc0b65c16610972f2278
박건규 <geongyu09@gmail.com>
2025-10-19 00:49:38 +0900

    feat(App): 성공 및 실패 결과 출력 로직 구현
    성공 및 실패시 요구사항에 맞추어서 올바른 결과물을 콘솔에 출력하도록 로직을 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
+import { Calculator } from "./Calculator";
 
 class App {
   async run() {
@@ -6,6 +7,11 @@ class App {
       const input = await Console.readLineAsync(
         "덧셈할 문자열을 입력해 주세요.\n",
       );
+
+      const calculator = new Calculator();
+
+      const result = calculator.calculate(input);
+      Console.print(`결과 : ${result}`);
     } catch (e) {
       throw new Error(e.message);
     }
