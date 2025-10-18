7f47770aab246743a4ab7b5a7be86a1503ce2cb2
박건규 <geongyu09@gmail.com>
2025-10-19 00:41:04 +0900

    feat(Calculator): 커스텀 구분자를 사용하지 않은 경우 처리 로직 구현
    커스텀 구분자를 사용하지 않은 경우는 전부 //가 아닌 문자로 시작하는 비어있지 않은 문자열입니다.
    
    알고리즘의 정상적인 플로우는 아래와 같습니다.
    - 입력한 글자를 하나씩 돕니다.
    - 글자가 숫자라면 buffer에 넣습니다.
    - 글자가 기본 구분자(",", ":")라면 지금까지 쌓인 buffer의 수를 arr에 넣습니다.
    - 마지막 문자에 도달했다면, buffer에 있는 수를 arr에 넣습니다.
    - arr에 있는 수를 순서대로 더하여 반환합니다.
    
    이때 buffer의 역할은 NumberBuffer클래스 인스턴스가 책임집니다.
    기본 구분자를 제외한 모든 문자는 숫자로 변환되어 NumberBuffer 인스턴스에 들어갑니다.
    이때 NumberBuffer 인스턴스는 올바르지 않는 값을 받으면 에러를 던지는 로직이 있습니다.
    이를 굳이 Calculator에서 이중으로 테스트 하기 않기 위하여 buffer.appendNumber(+char);를 try...catch로 감쌌습니다.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,3 +1,5 @@
+import { NumberBuffer } from "./NumberBuffer";
+
 export class Calculator {
   constructor() {}
 
@@ -23,5 +25,39 @@ export class Calculator {
         throw new Error("[ERROR] 부적절한 문자가 들어갔습니다.");
       return result;
     }
+
+    // 커스텀 구분자를 사용하지 않는 경우
+    if (
+      inputString.startsWith(":") ||
+      inputString.startsWith(",") ||
+      !Number.isNaN(parseInt(inputString[0]))
+    ) {
+      const buffer = new NumberBuffer();
+      const arr = []; // TODO: 이름 변경하기
+
+      inputString.split("").forEach((char, index) => {
+        if (char === ":" || char === ",") {
+          arr.push(buffer.toNumber());
+          buffer.clear();
+          return;
+        }
+
+        try {
+          buffer.appendNumber(+char);
+        } catch (e) {
+          throw new Error(`[ERROR] 올바르지 않는 문자가 들어있습니다. ${char}`);
+        }
+
+        if (index === inputString.length - 1) {
+          arr.push(buffer.toNumber());
+          buffer.clear();
+        }
+      });
+
+      const result = arr.reduce((sum, cur) => parseInt(sum) + parseInt(cur));
+      return result;
+    }
+
+    throw new Error("[ERROR]");
   }
 }
